1ade69f3278a0567ab7d3b386df9aa801275e0ab
HENG <102294114+amidhacksaw@users.noreply.github.com>
2025-05-23 23:15:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://amidhacksaw.github.io"}})
+CORS(app, origins=["https://amidhacksaw.github.io"], supports_credentials=True)
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 allowed_passwords = os.getenv("ALLOWED_PASSWORDS", "").split(",")
