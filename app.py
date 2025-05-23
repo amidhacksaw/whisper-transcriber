ecc316b33613450a96d44087b7c21ac61bca6c16
HENG <102294114+amidhacksaw@users.noreply.github.com>
2025-05-23 15:37:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -49,5 +49,4 @@ def transcribe():
             os.remove(filepath)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    print("Flask app is running in development mode.")
