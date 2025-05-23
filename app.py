f6d33be5debb61cbe67103ba1a62886785e3c3a3
HENG <102294114+amidhacksaw@users.noreply.github.com>
2025-05-23 15:22:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -49,4 +49,5 @@ def transcribe():
             os.remove(filepath)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
